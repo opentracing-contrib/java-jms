4996b398fa38195b3c2fb56c26b321f655ba7399
Revertegat Benjamin <10056395@gemalto.com>
2018-08-22 14:25:02 +0200

    prepare for pull request

--- opentracing-jms-2/src/main/java/io/opentracing/contrib/jms2/TracingJMSProducer.java
+++ opentracing-jms-2/src/main/java/io/opentracing/contrib/jms2/TracingJMSProducer.java
@@ -1,5 +1,10 @@
 package io.opentracing.contrib.jms2;
 
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.jms.common.SpanJmsDecorator;
+import io.opentracing.contrib.jms.common.TracingMessageUtils;
+
 import java.io.Serializable;
 import java.util.Map;
 import java.util.Set;
@@ -9,294 +14,291 @@ import javax.jms.Destination;
 import javax.jms.JMSProducer;
 import javax.jms.Message;
 
+import java.lang.UnsupportedOperationException;
+
 public class TracingJMSProducer implements JMSProducer {
 
-	@Override
-	public JMSProducer clearProperties() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public CompletionListener getAsync() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private final JMSProducer jmsProducer;
+    private final Tracer tracer;
 
-	@Override
-	public boolean getBooleanProperty(String arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public TracingJMSProducer(JMSProducer jmsProducer, Tracer tracer) {
+        this.jmsProducer = jmsProducer;
+        this.tracer = tracer;
+    }
 
-	@Override
-	public byte getByteProperty(String arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public JMSProducer clearProperties() {
+        jmsProducer.clearProperties();
+        return this;
+    }
 
-	@Override
-	public long getDeliveryDelay() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public CompletionListener getAsync() {
+        return jmsProducer.getAsync();
+    }
 
-	@Override
-	public int getDeliveryMode() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public boolean getBooleanProperty(String arg0) {
+        return jmsProducer.getBooleanProperty(arg0);
+    }
 
-	@Override
-	public boolean getDisableMessageID() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public byte getByteProperty(String arg0) {
+        return jmsProducer.getByteProperty(arg0);
+    }
 
-	@Override
-	public boolean getDisableMessageTimestamp() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public long getDeliveryDelay() {
+        return jmsProducer.getDeliveryDelay();
+    }
 
-	@Override
-	public double getDoubleProperty(String arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getDeliveryMode() {
+        return jmsProducer.getDeliveryMode();
+    }
 
-	@Override
-	public float getFloatProperty(String arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public boolean getDisableMessageID() {
+        return jmsProducer.getDisableMessageID();
+    }
 
-	@Override
-	public int getIntProperty(String arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public boolean getDisableMessageTimestamp() {
+        return jmsProducer.getDisableMessageTimestamp();
+    }
 
-	@Override
-	public String getJMSCorrelationID() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public double getDoubleProperty(String arg0) {
+        return jmsProducer.getDoubleProperty(arg0);
+    }
 
-	@Override
-	public byte[] getJMSCorrelationIDAsBytes() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public float getFloatProperty(String arg0) {
+        return jmsProducer.getFloatProperty(arg0);
+    }
 
-	@Override
-	public Destination getJMSReplyTo() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public int getIntProperty(String arg0) {
+        return jmsProducer.getIntProperty(arg0);
+    }
 
-	@Override
-	public String getJMSType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getJMSCorrelationID() {
+        return jmsProducer.getJMSCorrelationID();
+    }
 
-	@Override
-	public long getLongProperty(String arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public byte[] getJMSCorrelationIDAsBytes() {
+        return jmsProducer.getJMSCorrelationIDAsBytes();
+    }
 
-	@Override
-	public Object getObjectProperty(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Destination getJMSReplyTo() {
+        return jmsProducer.getJMSReplyTo();
+    }
 
-	@Override
-	public int getPriority() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public String getJMSType() {
+        return jmsProducer.getJMSType();
+    }
 
-	@Override
-	public Set<String> getPropertyNames() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public long getLongProperty(String arg0) {
+        return jmsProducer.getLongProperty(arg0);
+    }
 
-	@Override
-	public short getShortProperty(String arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public Object getObjectProperty(String arg0) {
+        return jmsProducer.getObjectProperty(arg0);
+    }
 
-	@Override
-	public String getStringProperty(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public int getPriority() {
+        return jmsProducer.getPriority();
+    }
 
-	@Override
-	public long getTimeToLive() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public Set<String> getPropertyNames() {
+        return jmsProducer.getPropertyNames();
+    }
 
-	@Override
-	public boolean propertyExists(String arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public short getShortProperty(String arg0) {
+        return jmsProducer.getShortProperty(arg0);
+    }
 
-	@Override
-	public JMSProducer send(Destination arg0, Message arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getStringProperty(String arg0) {
+        return jmsProducer.getStringProperty(arg0);
+    }
 
-	@Override
-	public JMSProducer send(Destination arg0, String arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public long getTimeToLive() {
+        return jmsProducer.getTimeToLive();
+    }
 
-	@Override
-	public JMSProducer send(Destination arg0, Map<String, Object> arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean propertyExists(String arg0) {
+        return jmsProducer.propertyExists(arg0);
+    }
 
-	@Override
-	public JMSProducer send(Destination arg0, byte[] arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer send(Destination destination, Message message) {
+        Span span = TracingMessageUtils.buildAndInjectSpan(destination, message, tracer);
+        try {
+            jmsProducer.send(destination, message);
+        } catch (Throwable e) {
+            SpanJmsDecorator.onError(e, span);
+            throw e;
+        } finally {
+            span.finish();
+        }
+        return this;
+    }
 
-	@Override
-	public JMSProducer send(Destination arg0, Serializable arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer send(Destination destination, String message) {
+        throw new UnsupportedOperationException(
+                "This send is not implemented yet");
+    }
 
-	@Override
-	public JMSProducer setAsync(CompletionListener arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer send(Destination destination, Map<String, Object> arg1) {
+        throw new UnsupportedOperationException(
+                "This send is not implemented yet");
+    }
 
-	@Override
-	public JMSProducer setDeliveryDelay(long arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer send(Destination destination, byte[] arg1) {
+        throw new UnsupportedOperationException(
+                "This send is not implemented yet");
+    }
 
-	@Override
-	public JMSProducer setDeliveryMode(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer send(Destination destination, Serializable arg1) {
+        throw new UnsupportedOperationException(
+                "This send is not implemented yet");
+    }
 
-	@Override
-	public JMSProducer setDisableMessageID(boolean arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setAsync(CompletionListener arg0) {
+        jmsProducer.setAsync(arg0);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setDisableMessageTimestamp(boolean arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setDeliveryDelay(long arg0) {
+        jmsProducer.setDeliveryDelay(arg0);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setJMSCorrelationID(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setDeliveryMode(int arg0) {
+        jmsProducer.setDeliveryMode(arg0);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setJMSCorrelationIDAsBytes(byte[] arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setDisableMessageID(boolean arg0) {
+        jmsProducer.setDisableMessageID(arg0);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setJMSReplyTo(Destination arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setDisableMessageTimestamp(boolean arg0) {
+        jmsProducer.setDisableMessageTimestamp(arg0);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setJMSType(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setJMSCorrelationID(String arg0) {
+        jmsProducer.setJMSCorrelationID(arg0);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setPriority(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setJMSCorrelationIDAsBytes(byte[] arg0) {
+        jmsProducer.setJMSCorrelationIDAsBytes(arg0);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setProperty(String arg0, boolean arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setJMSReplyTo(Destination destination) {
+        jmsProducer.setJMSReplyTo(destination);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setProperty(String arg0, byte arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setJMSType(String arg0) {
+        jmsProducer.setJMSType(arg0);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setProperty(String arg0, short arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setPriority(int arg0) {
+        jmsProducer.setPriority(arg0);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setProperty(String arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setProperty(String arg0, boolean arg1) {
+        jmsProducer.setProperty(arg0, arg1);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setProperty(String arg0, long arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setProperty(String arg0, byte arg1) {
+        jmsProducer.setProperty(arg0, arg1);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setProperty(String arg0, float arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setProperty(String arg0, short arg1) {
+        jmsProducer.setProperty(arg0, arg1);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setProperty(String arg0, double arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setProperty(String arg0, int arg1) {
+        jmsProducer.setProperty(arg0, arg1);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setProperty(String arg0, String arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setProperty(String arg0, long arg1) {
+        jmsProducer.setProperty(arg0, arg1);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setProperty(String arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setProperty(String arg0, float arg1) {
+        jmsProducer.setProperty(arg0, arg1);
+        return this;
+    }
 
-	@Override
-	public JMSProducer setTimeToLive(long arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public JMSProducer setProperty(String arg0, double arg1) {
+        jmsProducer.setProperty(arg0, arg1);
+        return this;
+    }
+
+    @Override
+    public JMSProducer setProperty(String arg0, String arg1) {
+        jmsProducer.setProperty(arg0, arg1);
+        return this;
+    }
+
+    @Override
+    public JMSProducer setProperty(String arg0, Object arg1) {
+        jmsProducer.setProperty(arg0, arg1);
+        return this;
+    }
+
+    @Override
+    public JMSProducer setTimeToLive(long arg0) {
+        jmsProducer.setTimeToLive(arg0);
+        return this;
+    }
 
 }
