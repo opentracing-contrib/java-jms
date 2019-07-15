d46855e353518beed4af12b1c54e7c50aab3c1fd
lucapreziati <luca.preziati@gmail.com>
2019-07-15 16:38:27 +0200

    fix for permit tracing on sendAndReceive

--- opentracing-jms-spring/src/main/java/io/opentracing/contrib/jms/spring/TracingJmsTemplate.java
+++ opentracing-jms-spring/src/main/java/io/opentracing/contrib/jms/spring/TracingJmsTemplate.java
@@ -13,43 +13,90 @@
  */
 package io.opentracing.contrib.jms.spring;
 
-
 import io.opentracing.Tracer;
 import io.opentracing.contrib.jms.TracingMessageProducer;
 import io.opentracing.contrib.jms.common.TracingMessageConsumer;
+import io.opentracing.util.GlobalTracer;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
+import javax.jms.TemporaryQueue;
+
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.jms.support.JmsUtils;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 
 // Decorator for Spring JmsTemplate
 public class TracingJmsTemplate extends JmsTemplate {
 
-  private final Tracer tracer;
+	private final Tracer tracer;
 
-  public TracingJmsTemplate(Tracer tracer) {
-    this.tracer = tracer;
-  }
+	/**
+	 * GlobalTracer is used to get tracer
+	 */
+	public TracingJmsTemplate() {
+		this(GlobalTracer.get());
+	}
 
-  public TracingJmsTemplate(ConnectionFactory connectionFactory, Tracer tracer) {
-    super(connectionFactory);
-    this.tracer = tracer;
-  }
+	public TracingJmsTemplate(Tracer tracer) {
+		this.tracer = tracer;
+	}
 
-  @Override
-  protected MessageProducer createProducer(Session session, Destination destination)
-      throws JMSException {
-    return new TracingMessageProducer(super.createProducer(session, destination), tracer);
-  }
+	/**
+	 * GlobalTracer is used to get tracer
+	 */
+	public TracingJmsTemplate(ConnectionFactory connectionFactory) {
+		this(connectionFactory, GlobalTracer.get());
+	}
 
-  @Override
-  protected MessageConsumer createConsumer(Session session, Destination destination,
-      String messageSelector)
-      throws JMSException {
-    return new TracingMessageConsumer(super.createConsumer(session, destination, messageSelector),
-        tracer);
-  }
+	public TracingJmsTemplate(ConnectionFactory connectionFactory, Tracer tracer) {
+		super(connectionFactory);
+		this.tracer = tracer;
+	}
+
+	@Override
+	protected MessageProducer createProducer(Session session, Destination destination) throws JMSException {
+
+		return new TracingMessageProducer(super.createProducer(session, destination), tracer);
+	}
+
+	@Override
+	protected MessageConsumer createConsumer(Session session, Destination destination, String messageSelector)
+			throws JMSException {
+		return new TracingMessageConsumer(super.createConsumer(session, destination, messageSelector), tracer);
+	}
+
+	@Nullable
+	protected Message doSendAndReceive(Session session, Destination destination, MessageCreator messageCreator)
+			throws JMSException {
+
+		Assert.notNull(messageCreator, "MessageCreator must not be null");
+		TemporaryQueue responseQueue = null;
+		MessageProducer producer = null;
+		MessageConsumer consumer = null;
+		try {
+			Message requestMessage = messageCreator.createMessage(session);
+			responseQueue = session.createTemporaryQueue();
+			producer = new TracingMessageProducer(session.createProducer(destination));
+			consumer = new TracingMessageConsumer(session.createConsumer(responseQueue));
+			requestMessage.setJMSReplyTo(responseQueue);
+			if (logger.isDebugEnabled()) {
+				logger.debug("Sending created message: " + requestMessage);
+			}
+			doSend(producer, requestMessage);
+			return receiveFromConsumer(consumer, getReceiveTimeout());
+		} finally {
+			JmsUtils.closeMessageConsumer(consumer);
+			JmsUtils.closeMessageProducer(producer);
+			if (responseQueue != null) {
+				responseQueue.delete();
+			}
+		}
+	}
 }
